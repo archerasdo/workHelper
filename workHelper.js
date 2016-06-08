b9c39ca66c6dbde70225dbd3e614a0994164311d
gongjian.gj <gongjian.gj@alibaba-inc.com>
2016-06-08 16:06:47 +0800

    add helper

--- workHelper.js
+++ workHelper.js
@@ -1,16 +1,51 @@
 #!/usr/bin/env node
-
 'use strict'
-const fs = require('fs');
 
+const fs = require('fs');
+const config = require('./package.json');
 
 class workHelper {
     constructor() {
 
     }
 
-    getArgv(){
-        return process.argv[2];
+    getFeatureName() {
+        return process.argv[2]
+    }
+
+    getFeaturePath() {
+        return process.argv[3];
+    }
+
+    getCfgPath() {
+        return config.cfgPath;
+    }
+
+    parseFile() {
+        let path = this.getCfgPath();
+        if (fs.existsSync(path)) {
+            let data = fs.readFileSync(path).toString();
+            data = data.replace(/(\/\/.*)|(\/\*[\s\S]*?\*\/)/g,'');
+            let cfg = data.match(/cfg[\s+]=[\s+]({[^}]*})/i);
+            if (cfg) {
+                cfg = JSON.parse(cfg[1]);
+                cfg[this.getFeatureName()] = this.getFeaturePath();
+                cfg = JSON.stringify(cfg, null, "\t");
+                console.log(cfg);
+            } else {
+                let newCfg = {};
+                newCfg[this.getFeatureName()] = this.getFeatureName();
+                newCfg = JSON.stringify(newCfg, null, "\t");
+                console.log(newCfg)
+            }
+        }
+    }
+
+    parseLine(text) {
+
     }
 }
 
+var worker = new workHelper();
+
+worker.parseFile();
