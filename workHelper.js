09e1d99818acb9fe0b6745e99f4656d7a6691fa7
gongjian.gj <gongjian.gj@alibaba-inc.com>
2016-06-12 16:12:53 +0800

    featrue complete

--- workHelper.js
+++ workHelper.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
-'use strict'
+'use strict';
 
 const fs = require('fs');
 const config = require('./package.json');
+const p = require('path');
+const callCmd = require('child_process');
+
 
 class workHelper {
     constructor() {
-
+        if()
     }
 
     getFeatureName() {
@@ -21,36 +24,71 @@ class workHelper {
         return config.cfgPath;
     }
 
+    //删除行内注释和段落注释
+    deleteCodeComments(data) {
+        // http://排除方式,将'://'全部替换为特殊字符，删除注释代码后再将其恢复回来
+        const tmp1 = ':\/\/';
+        const regTmp1 = /:\/\//g;
+        const tmp2 = '@:@/@/@';
+        const regTmp2 = /@:@\/@\/@/g;
+        data = data.replace(regTmp1, tmp2);
+        const reg = /(\/\/.*)|(\/\*[\s\S]*?\*\/)/g;
+        data = data.replace(reg, '');
+        return data.replace(regTmp2, tmp1);
+    }
+
+    //调整cfg对象,将新增分支目录写入cfg对象
+    parseCfg(data) {
+        let cfg = data.match(/cfg[\s+]=[\s+]({[^}]*})/i);
+        if (cfg) {
+            cfg = JSON.parse(cfg[1]);
+            cfg[this.getFeatureName()] = this.getFeaturePath();
+            cfg = JSON.stringify(cfg, null, "\t");
+            return data.replace(/cfg[\s+]=[\s+]({[^}]*})/,function(m,p1){
+                return "cfg = " + cfg;
+            });
+        } else {
+            cfg = {};
+            cfg[this.getFeatureName()] = this.getFeaturePath();
+            cfg = JSON.stringify(cfg, null, "\t");
+            return 'cfg = '+ cfg + data;
+        }
+
+    }
+
+    parseStyleRoot(data) {
+        return data.replace(/root[\s]?:[\s]?(.*)/g,'root: cfg["'+this.getFeatureName() + '"],');
+    }
+
+    writeFile(path,data){
+        fs.renameSync(path,path+'.default');
+        let buffer = new Buffer(data);
+        fs.writeFile(path,buffer,function(err){
+            if(err) throw err;
+            console.log('write finished');
+        });
+    }
+
+    startFdServer(){
+        callCmd.execFile('server.sh',function(error,stdout,stderr){
+            if (error) {
+                console.log('stderr:'+stderr)
+            }
+        })
+    }
+
     parseFile() {
         let path = this.getCfgPath();
         if (fs.existsSync(path)) {
             let data = fs.readFileSync(path).toString();
-            data = data.replace(/(\/\/.*)|(\/\*[\s\S]*?\*\/)/g,'');
-            let cfg = data.match(/cfg[\s+]=[\s+]({[^}]*})/i);
-            if (cfg) {
-                cfg = JSON.parse(cfg[1]);
-                cfg[this.getFeatureName()] = this.getFeaturePath();
-                cfg = JSON.stringify(cfg, null, "\t");
-            } else {
-                cfg = {};
-                cfg[this.getFeatureName()] = this.getFeatureName();
-                cfg = JSON.stringify(cfg, null, "\t");
-            }
-            data.replace(/cfg[\s+]=[\s+]({[^}]*})/,'');
-            //let writeString = "var cfg = " + cfg;
-            let buffer = new Buffer(data);
-            fs.writeFile('./test.js',buffer,function(err){
-                if(err) throw err;
-                console.log('write finished');
-            })
+            data = this.deleteCodeComments(data);
+            data = this.parseCfg(data);
+            data = this.parseStyleRoot(data);
+            this.writeFile(path,data);
+            this.startFdServer();
         }
-    }
-
-    parseLine(text) {
-
     }
 }
 
 var worker = new workHelper();
-
 worker.parseFile();
